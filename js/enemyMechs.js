a15b75cda66fc8871cb37907ffe5741063a0aa5b
Jufebrown <jufebrown@gmail.com>
2017-02-10 12:51:04 -0600

    changed filename

---
+++ js/enemyMechs.js
@@ -0,0 +1,17 @@
+/***************************************
+MECHS
+***************************************/
+
+EnemyScout = function(game,x,y) {
+  Phaser.Sprite.call(this, game, x, y, "enemy");
+  this.name = "Scout";
+  this.movePoints = 4
+  this.weapon = "SMG"
+  this.weaponRange = 2
+  this.health = Math.floor((Math.random() * 50) + 200)
+  this.damage = Math.floor((Math.random() * 50) + 200)
+  this.visible = true
+  enemySquad.add(this);
+};
+EnemyScout.prototype = Object.create(Phaser.Sprite.prototype);
+EnemyScout.prototype.constructor = EnemyScout;
