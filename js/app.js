253b729e6a7f1bb69cc79c24da4f2d2473d7562d
Jufebrown <jufebrown@gmail.com>
2017-02-13 14:48:01 -0600

    made a function to draw status rectangles

--- js/app.js
+++ js/app.js
@@ -116,8 +116,32 @@ function onCreate() {
 
   startPlayerTurn()
 
+  drawHUD()
+
   // game.input.onDown.add(gofull, this);
   this.camera.flash('#000000', 2000);
+}
+
+function drawHUD() {
+  let infoHUD = game.add.graphics(100, 100);
+
+  // set a fill and line style again
+  infoHUD.beginFill(0x2176ff, 1);
+
+  // draw a rectangle
+  infoHUD.drawRect(380, -60, 150, 200);
+  infoHUD.endFill();
+
+  // set a fill and line style again
+  infoHUD.beginFill(0xd1250e, 1);
+
+  // draw a rectangle
+  infoHUD.drawRect(380, 150, 150, 200);
+  infoHUD.endFill();
+
+  infoHUD.alpha = .6
+  infoHUD.fixedToCamera = true
+  window.graphics = infoHUD;
 }
 
 function enablePlayerMoves() {
