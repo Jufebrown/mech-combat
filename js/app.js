34b43ca59237297134630067dea2e44f4dd44246
Jufebrown <jufebrown@gmail.com>
2017-02-14 15:08:17 -0600

    modularized turn management functions

--- js/app.js
+++ js/app.js
@@ -67,39 +67,6 @@ let explosions
 let explosionSound
 
 
-function startPlayerTurn() {
-  enablePlayerMoves()
-}
-
-function checkEndPlayerTurn() {
-  if (checkPlayerDone) {
-    enemyTurnSetup()
-    startEnemyTurn()
-  }
-}
-
-function enemyTurnSetup() {
-  for(var i = 0, length1 = enemySquad.children.length; i < length1; i++){
-    enemySquad.children[i].hasMoved = false
-    enemySquad.children[i].hasFired = false
-  }
-}
-
-function checkPlayerDone() {
-  let allPlayersDone = true
-  for(var i = 0, length1 = playerSquad.chidren.length; i < length1; i++){
-    if(playerSquad.chidren[i].hasFired === false) {
-      allPlayersDone = false
-    }
-  }
-  return allPlayersDone
-}
-
-function startEnemyTurn() {
-  if (!victory) {
-    enemyMoveType()
-  }
-}
 
 function enemyMoveType() {
   for(var i = 0, length1 = enemySquad.children.length; i < length1; i++){
