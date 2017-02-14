930fe6d960b027a3759aa250539f50db43821960
Jufebrown <jufebrown@gmail.com>
2017-02-14 15:03:57 -0600

    modularized mech explosion functions

--- js/app.js
+++ js/app.js
@@ -63,32 +63,10 @@ let turnCounter = 1
 let targetFound = false
 let moveToCubePos
 let victory = false
-
-
 let explosions
 let explosionSound
 
 
-
-function setupExplosion (explosions) {
-
-    explosions.anchor.x = 0.5;
-    explosions.anchor.y = 0.5;
-    explosions.animations.add('kaboom');
-
-}
-
-function start() {
-  console.log('explosion sound is ready')
-}
-
-function explodeMech(target) {
-  game.world.bringToTop(explosions)
-  let explosion = explosions.getFirstExists(false);
-  explosion.reset(target.body.x+20, target.body.y+20);
-  explosion.play('kaboom', 20, false, true);
-  explosionSound.play()
-}
 
 function disablePlayerMoves() {
   for(var i = 0, length1 = playerSquad.children.length; i < length1; i++){
---
+++ js/mech-explosion.js
@@ -0,0 +1,17 @@
+function setupExplosion (explosions) {
+    explosions.anchor.x = 0.5;
+    explosions.anchor.y = 0.5;
+    explosions.animations.add('kaboom');
+}
+
+function start() {
+  console.log('explosion sound is ready')
+}
+
+function explodeMech(target) {
+  game.world.bringToTop(explosions)
+  let explosion = explosions.getFirstExists(false);
+  explosion.reset(target.body.x+20, target.body.y+20);
+  explosion.play('kaboom', 20, false, true);
+  explosionSound.play()
+}
