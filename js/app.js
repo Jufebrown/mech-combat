0ddd8fbc8b1febda3d5b3e7e8bb3fa5effd1d543
Jufebrown <jufebrown@gmail.com>
2017-02-14 15:05:44 -0600

    modularized toggle player moves

--- js/app.js
+++ js/app.js
@@ -67,22 +67,6 @@ let explosions
 let explosionSound
 
 
-
-function disablePlayerMoves() {
-  for(var i = 0, length1 = playerSquad.children.length; i < length1; i++){
-    playerSquad.children[i].events.onInputDown.remove(getMoveRange, playerSquad.children[i])
-  }
-}
-
-
-function enablePlayerMoves() {
-  for(var i = 0, length1 = playerSquad.children.length; i < length1; i++){
-    if (playerSquad.children[i].hasMoved === false) {
-      playerSquad.children[i].events.onInputDown.add(getMoveRange, playerSquad.children[i])
-    }
-  }
-}
-
 function startPlayerTurn() {
   enablePlayerMoves()
 }
