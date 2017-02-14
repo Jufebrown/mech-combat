81dc86c9d38c42323122697a05cb7ab748ebe400
Jufebrown <jufebrown@gmail.com>
2017-02-14 14:29:12 -0600

    added hexPosition function that gets hex position from sprite position

--- js/position.js
+++ js/position.js
@@ -29,3 +29,34 @@ function hexPosition() {
   // console.log('hexPositionObj', hexPositionObj)
   return hexPositionObj
 }
+
+function hexPositionFromSpriteCoordinates(spriteX, spriteY) {
+  let hexPositionObj = {}
+  hexPositionObj.x = Math.floor((spriteX-hexagonGroup.x)/sectorWidth);
+  hexPositionObj.y = Math.floor((spriteY-hexagonGroup.y)/sectorHeight);
+  let deltaX = (spriteX-hexagonGroup.x)%sectorWidth;
+  let deltaY = (spriteY-hexagonGroup.y)%sectorHeight;
+  if(hexPositionObj.x%2==0){
+    if(deltaX<((hexagonWidth/4)-deltaY*gradient)){
+      hexPositionObj.x--;
+      hexPositionObj.y--;
+    }
+    if(deltaX<((-hexagonWidth/4)+deltaY*gradient)){
+      hexPositionObj.x--;
+    }
+  } else {
+    if(deltaY>=hexagonHeight/2) {
+      if(deltaX<(hexagonWidth/2-deltaY*gradient)){
+        hexPositionObj.x--;
+      }
+    } else {
+      if(deltaX<deltaY*gradient){
+        hexPositionObj.x--;
+      } else {
+        hexPositionObj.y--;
+      }
+    }
+  }
+  // console.log('hexPositionObj', hexPositionObj)
+  return hexPositionObj
+}
