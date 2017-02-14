05fb26799fff62a9d934a0a4d8b1e3781dc57d03
Jufebrown <jufebrown@gmail.com>
2017-02-14 14:07:03 -0600

    altered disablePlayerMoves

--- js/app.js
+++ js/app.js
@@ -163,6 +163,13 @@ function explodeMech(target) {
   explosionSound.play()
 }
 
+function disablePlayerMoves() {
+  for(var i = 0, length1 = playerSquad.children.length; i < length1; i++){
+    playerSquad.children[i].events.onInputDown.remove(getMoveRange, playerSquad.children[i])
+  }
+}
+
+
 function enablePlayerMoves() {
   for(var i = 0, length1 = playerSquad.children.length; i < length1; i++){
     if (playerSquad.children[i].hasMoved === false) {
