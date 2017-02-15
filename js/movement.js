55d9847f907a804ab6204cd38c959347b3004c0e
Jufebrown <jufebrown@gmail.com>
2017-02-15 10:53:10 -0600

    made player sprites face hex they are moving toward

--- js/movement.js
+++ js/movement.js
@@ -11,7 +11,7 @@ function moveSprite (posX,posY) {
   let endX = hexToPixelX(posX)
   let endY = hexToPixelY(posX,posY)
   moveToCubePos = offsetToCube(hexPosition().x,hexPosition().y)
-
+  currentSprite.rotation = game.physics.arcade.angleToPointer(currentSprite)
   //  300 = 300 pixels per second = the speed the sprite will move at, regardless of the distance it has to travel
   var duration = 1000 //(game.physics.arcade.distanceToPointer(player, pointer) / 300) * 1000;
   tween = game.add.tween(currentSprite).to({ x: endX, y: endY }, duration, Phaser.Easing.Linear.None, true);
