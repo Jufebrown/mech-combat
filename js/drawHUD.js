f4bb6e3173c248aff01eba1e54374eb7ae39acf6
Jufebrown <jufebrown@gmail.com>
2017-02-16 23:06:06 -0600

    commented out hud code

--- js/drawHUD.js
+++ js/drawHUD.js
@@ -1,21 +1,21 @@
 function drawHUD() {
-  let infoHUD = game.add.graphics(100, 100);
+  // let infoHUD = game.add.graphics(100, 100);
 
-  // set a fill and line style again
-  infoHUD.beginFill(0x2176ff, 1);
+  // // set a fill and line style again
+  // infoHUD.beginFill(0x2176ff, 1);
 
-  // draw a rectangle
-  infoHUD.drawRect(380, -60, 150, 200);
-  infoHUD.endFill();
+  // // draw a rectangle
+  // infoHUD.drawRect(380, -60, 150, 200);
+  // infoHUD.endFill();
 
-  // set a fill and line style again
-  infoHUD.beginFill(0xd1250e, 1);
+  // // set a fill and line style again
+  // infoHUD.beginFill(0xd1250e, 1);
 
-  // draw a rectangle
-  infoHUD.drawRect(380, 150, 150, 200);
-  infoHUD.endFill();
+  // // draw a rectangle
+  // infoHUD.drawRect(380, 150, 150, 200);
+  // infoHUD.endFill();
 
-  infoHUD.alpha = .6
-  infoHUD.fixedToCamera = true
-  window.graphics = infoHUD;
+  // infoHUD.alpha = .6
+  // infoHUD.fixedToCamera = true
+  // window.graphics = infoHUD;
 }
