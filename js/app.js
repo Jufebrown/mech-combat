c784f240e4439e3c81c1709da879d1e5ec75ef97
Jufebrown <jufebrown@gmail.com>
2017-02-16 23:25:47 -0600

    made an enemy tactics script

--- js/app.js
+++ js/app.js
@@ -94,34 +94,7 @@ function enemyMoveType() {
   }
 }
 
-function chargeAtPlayer() {
-  let nearestPlayer = findNearestPlayer()
-  // console.log('nearestPlayer', nearestPlayer)
-  // console.log('currentlyMovingEnemy', currentlyMovingEnemy)
-  let positionCurrentEnemy = hexPositionFromSpriteCoordinates(currentlyMovingEnemy.x, currentlyMovingEnemy.y)
-  let cubePositionCurrentEnemy = offsetToCube(positionCurrentEnemy.x, positionCurrentEnemy.y)
-  //get position of nearest player
-  let nearestPlayerHexPos = hexPositionFromSpriteCoordinates(nearestPlayer.x, nearestPlayer.y)
-  let cubeNearestPlayerPos = offsetToCube(nearestPlayerHexPos.x, nearestPlayerHexPos.y)
-  //get distance to nearest player
-  let distanceBetweenEnemyAndNearestPlayer = cubeDistance(cubePositionCurrentEnemy, cubeNearestPlayerPos)
-  // console.log('cubeDistanceBetweenEnemyAndNearestPlayer', distanceBetweenEnemyAndNearestPlayer)
-  //if nearest player is in weapon range => attack
-  if (distanceBetweenEnemyAndNearestPlayer <= currentlyMovingEnemy.weaponRange) {
-    // fire on nearestPlayer
-    currentlyMovingEnemy.hasMoved = true
-    enemyAttack()
-  } else { //else move to hex closest to nearest player
-    //get enemy move range
-    let enemyMoveCubeRangeArray = getEnemyMoveRange(cubePositionCurrentEnemy)
-    // console.log('enemyMoveCubeRangeArray', enemyMoveCubeRangeArray)
-    //test each range hex to see if it's closest to nearest player
-    let cubeNearestHex = findNearestHex(cubeNearestPlayerPos, enemyMoveCubeRangeArray)
-    // console.log('cubeNearestHex', cubeNearestHex)
-    let offsetNearestHex = cubeToOffset(cubeNearestHex.x, cubeNearestHex.z)
-    moveEnemySprite(offsetNearestHex, nearestPlayer)
-  }
-}
+
 
 //moves sprite to specified hex
 function moveEnemySprite(offsetNearestHex, nearestPlayer) {
