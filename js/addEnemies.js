95ee4894fd8bf7e16627db8c76c8326a93bef0dc
Jufebrown <jufebrown@gmail.com>
2017-04-14 15:53:13 -0500

    added comment

--- js/addEnemies.js
+++ js/addEnemies.js
@@ -6,10 +6,12 @@ function addEnemySquad() {
     {positionX: 8, positionY: 0}
     ]
 
+  // loops over array and puts each unit on x,y coordinates
   for(let i = 0, length1 = startingEnemySquadArray.length; i < length1; i++){
     let startX = hexToPixelX(startingEnemySquadArray[i].positionX)
     let startY = hexToPixelY(startingEnemySquadArray[i].positionX,startingEnemySquadArray[i].positionY)
     new EnemyScout(game, startX, startY)
+    //allows squad units to be clickable
     enemySquad.children[i].inputEnabled = true
   }
 }
