9d71ccf39314fb258b945f0c390cedadd829d9c7
Jufebrown <jufebrown@gmail.com>
2017-04-14 22:13:21 -0500

    added comments

--- js/combat.js
+++ js/combat.js
@@ -21,21 +21,25 @@ function targetEnable(targetCandidate) {
                 Enemy
 **************************************/
 
-
+// checks to see if unit health is <= 0
 function destroyCheck(target) {
   if (target.health <= 0) {
     explodeMech(target)
+    // calls kill and destroy (phaser methods)
     target.kill()
     target.destroy()
+    // removes unit from group
     if (target.key === 'player') {
       playerSquad.remove(target)
     } else if (target.key === 'enemy') {
       enemySquad.remove(target)
     }
+    // calls a check on game over
     gameOverCheck()
   }
 }
 
+// function to find hexes within weapon range
 function getWeaponRange() {
   let startCubePosition = moveToCubePos
   let nRange = currentSprite.weaponRange
@@ -44,23 +48,30 @@ function getWeaponRange() {
   highlightRange(cubeFireRange, nextAction)
 }
 
-
+// removes click listener
 function targetDisable(targetCandidate) {
   targetCandidate.events.onInputDown.remove(combat, targetCandidate)
 }
 
+// calculates weapon hit
 function hitCalc() {
+  // initializes to miss
   let hitResolution = {hit: false, crit: false}
+  //gets a random number between 1-100
   let hitRoll = Math.floor(Math.random() * 100) + 1
-  console.log('hitRoll', hitRoll)
+  // console.log('hitRoll', hitRoll)
+  // need at least the toHitNumber to hit target
   let toHitNumber = 20
+  // critical hit if attack roll is 90+
   if (hitRoll >= 90) {
     hitResolution.hit = true
     hitResolution.crit = true
     return hitResolution
+  // hit if attack roll >= toHitNumber
   } else if (hitRoll >= toHitNumber) {
     hitResolution.hit = true
     return hitResolution
+  // miss if attack roll lower than toHitNumber
   } else {
     return hitResolution
   }
