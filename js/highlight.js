ef135ae6d5590885e9551e05caa93fddd7bac4e5
Jufebrown <jufebrown@gmail.com>
2017-04-17 09:19:57 -0500

    added comments

--- js/highlight.js
+++ js/highlight.js
@@ -1,20 +1,31 @@
 /**************************************
                 Player
 **************************************/
+// highlights hexes in range of unit
 function highlightRange(cubeRange, nextAction) {
+  // adds highlight group
   highlightGroup = game.add.group()
+  // sets offset
   highlightGroup.x = hexagonGroup.x
   highlightGroup.y = hexagonGroup.y
+  // sets layer above map layer
   highlightGroup.z = 1
+  // sets targetFound variable to default at false
   targetFound = false
+  // loops through hexes in range
   for(var i = 0, length1 = cubeRange.length; i < length1; i++){
+    // sets up position to place highlighted hex
     let currentHex = cubeToOffset(cubeRange[i].x, cubeRange[i].z)
     let startX = hexToPixelX(currentHex.col)
     let startY = hexToPixelY(currentHex.col,currentHex.row)
+    // instantiates highlight sprite
     new Highlight(game, startX, startY)
+    // allows highlight hex to accept input
     highlightGroup.children[i].inputEnabled = true
+    // enables physics to work on new hex, this allows a check to see if there is overlap with another physics body
     game.physics.enable(highlightGroup.children[i], Phaser.Physics.ARCADE)
     highlightGroup.children[i].body.setSize(32, 32, 0, 0)
+    // checks to see if next action being taken is movement or firing
     if (nextAction === 'move') {
       highlightGroup.children[i].events.onInputDown.add(checkHex, highlightGroup.children[i])
     } else if (nextAction === 'fire') {
@@ -23,6 +34,7 @@ function highlightRange(cubeRange, nextAction) {
   }
 }
 
+// turns off highlight hexes
 function killHighlight() {
   for(let i = 0, length1 = highlightGroup.children.length; i < length1; i++){
     highlightGroup.children[i].visible = false
@@ -30,16 +42,19 @@ function killHighlight() {
   highlightGroup.children = []
 }
 
+// tints highlight hexes red
 function spriteTint(highlightSprite) {
   targetFound = true
   highlightSprite.tint = 0xff2100
   highlightSprite.alpha = .3
+  // loops over enemy squad and checks to see if they are within highlighted hexes
   for(let i = 0, length1 = enemySquad.children.length; i < length1; i++){
     let targetCandidate = enemySquad.children[i]
     game.physics.arcade.overlap(targetCandidate, highlightSprite, this.targetEnable, null, this)
   }
 }
 
+// checks to see if enemy units are within highlight hexes
 function targetCheck(highlightSprite) {
   game.physics.arcade.overlap(highlightSprite, enemySquad, this.spriteTint, null, this)
   game.time.events.add(Phaser.Timer.SECOND * .5, resolveTargetNotFound, this)
