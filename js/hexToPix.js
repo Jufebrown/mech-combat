0166e4bcd963b35b6c17af9b72b656f359a7d47b
Jufebrown <jufebrown@gmail.com>
2017-04-17 09:06:58 -0500

    added comments

--- js/hexToPix.js
+++ js/hexToPix.js
@@ -1,10 +1,11 @@
 // functions to convert odd-q hex coodinates to pixel coordinates
-
+// converts x coordinate
 function hexToPixelX(posX) {
-  let pixelX = hexagonWidth/4*3*posX+hexagonWidth/2;
+  let pixelX = (hexagonWidth/4)*3*posX+hexagonWidth/2;
   return pixelX
 }
 
+// converts y coordinate
 function hexToPixelY(posX, posY) {
   let pixelY = hexagonHeight*posY;
   if(posX%2==0){
