eb37089601478d971259ab6797652e5c1bf3b5cf
Jufebrown <jufebrown@gmail.com>
2018-02-01 13:49:15 -0600

    exported rangeCalc.js

--- js/math/rangeCalc.js
+++ js/math/rangeCalc.js
@@ -1,18 +1,23 @@
-function rangeCalc(startCubePosition, nRange) {
-  let rangeResults = []
-  let xMin = startCubePosition.x-nRange
-  let xMax = startCubePosition.x+nRange
-  let yMin = startCubePosition.y-nRange
-  let yMax = startCubePosition.y+nRange
-  let zMin = startCubePosition.z-nRange
-  let zMax = startCubePosition.z+nRange
-  let i = 0
-  for (let x = xMin; x <= xMax; x++) {
-    for (let y = Math.max(yMin, -x-zMax); y <= Math.min(yMax, -x-zMin); y++) {
-        var z = -x-y;
-        rangeResults[i] = {x: x, y: y, z: z,};
-        i++
+'use strict';
+
+module.exports = function() {
+  function rangeCalc(startCubePosition, nRange) {
+    let rangeResults = []
+    let xMin = startCubePosition.x-nRange
+    let xMax = startCubePosition.x+nRange
+    let yMin = startCubePosition.y-nRange
+    let yMax = startCubePosition.y+nRange
+    let zMin = startCubePosition.z-nRange
+    let zMax = startCubePosition.z+nRange
+    let i = 0
+    for (let x = xMin; x <= xMax; x++) {
+      for (let y = Math.max(yMin, -x-zMax); y <= Math.min(yMax, -x-zMin); y++) {
+          var z = -x-y;
+          rangeResults[i] = {x: x, y: y, z: z,};
+          i++
+      }
     }
+    return rangeResults
   }
-  return rangeResults
 }
+
