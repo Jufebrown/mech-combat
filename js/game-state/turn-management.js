493f129756ae8d001de14335e8242477ba8f99af
Jufebrown <jufebrown@gmail.com>
2018-02-01 13:39:03 -0600

    exported turn-management.js

--- js/game-state/turn-management.js
+++ js/game-state/turn-management.js
@@ -1,101 +1,105 @@
-/**************************************
-                Player
-**************************************/
+'use strict';
 
-function startPlayerTurn(playerTurnText) {
-  playerTurnText.destroy()
-  game.world.bringToTop(playerSquad)
-  enablePlayerMoves()
-}
-
-function playerTurnSetup() {
-  for(var i = 0, length1 = playerSquad.children.length; i < length1; i++){
-    playerSquad.children[i].hasMoved = false
-    playerSquad.children[i].hasFired = false
+module.exports = function() {
+  /**************************************
+                  Player
+  **************************************/
+  
+  function startPlayerTurn(playerTurnText) {
+    playerTurnText.destroy()
+    game.world.bringToTop(playerSquad)
+    enablePlayerMoves()
   }
-}
-
-function checkEndPlayerTurn() {
-  let playerDone = checkPlayerDone()
-  console.log('checking player turn end', playerDone)
-  if (playerDone) {
-    // gameOverCheck()
-    if (!gameOver) {
-      playerTurn = false
-      enemyTurnSetup()
-      let enemyTurnText = game.add.text(300, 200, "Enemy Turn Start");
-      enemyTurnText.anchor.set(0.5);
-      enemyTurnText.align = 'center';
-      enemyTurnText.font = 'Arial';
-      enemyTurnText.fontWeight = 'bold';
-      enemyTurnText.fontSize = 40;
-      enemyTurnText.fill = '#ffffff';
-      enemyTurnText.fixedToCamera = true
-      game.time.events.add(Phaser.Timer.SECOND * 1, startEnemyTurn, this, enemyTurnText);
+  
+  function playerTurnSetup() {
+    for(var i = 0, length1 = playerSquad.children.length; i < length1; i++){
+      playerSquad.children[i].hasMoved = false
+      playerSquad.children[i].hasFired = false
     }
   }
-}
-
-function checkPlayerDone() {
-  let allPlayersDone = true
-  for(var i = 0, length1 = playerSquad.children.length; i < length1; i++){
-    if(playerSquad.children[i].hasFired === false) {
-      allPlayersDone = false
-      return allPlayersDone
+  
+  function checkEndPlayerTurn() {
+    let playerDone = checkPlayerDone()
+    console.log('checking player turn end', playerDone)
+    if (playerDone) {
+      // gameOverCheck()
+      if (!gameOver) {
+        playerTurn = false
+        enemyTurnSetup()
+        let enemyTurnText = game.add.text(300, 200, "Enemy Turn Start");
+        enemyTurnText.anchor.set(0.5);
+        enemyTurnText.align = 'center';
+        enemyTurnText.font = 'Arial';
+        enemyTurnText.fontWeight = 'bold';
+        enemyTurnText.fontSize = 40;
+        enemyTurnText.fill = '#ffffff';
+        enemyTurnText.fixedToCamera = true
+        game.time.events.add(Phaser.Timer.SECOND * 1, startEnemyTurn, this, enemyTurnText);
+      }
     }
   }
-  return allPlayersDone
-}
-
-
-/**************************************
-                Enemy
-**************************************/
-
-function checkEndEnemyTurn() {
-  let enemyDone = checkEnemyDone()
-  console.log('enemyDone', enemyDone)
-  if (enemyDone) {
-    // gameOverCheck()
-    if (!gameOver) {
-      playerTurn = true
-      playerTurnSetup()
-      let playerTurnText = game.add.text(300, 200, "Player Turn Start");
-      playerTurnText.anchor.set(0.5);
-      playerTurnText.align = 'center';
-      playerTurnText.font = 'Arial';
-      playerTurnText.fontWeight = 'bold';
-      playerTurnText.fontSize = 40;
-      playerTurnText.fill = '#ffffff';
-      playerTurnText.fixedToCamera = true
-      game.time.events.add(Phaser.Timer.SECOND * 1, startPlayerTurn, this, playerTurnText);
+  
+  function checkPlayerDone() {
+    let allPlayersDone = true
+    for(var i = 0, length1 = playerSquad.children.length; i < length1; i++){
+      if(playerSquad.children[i].hasFired === false) {
+        allPlayersDone = false
+        return allPlayersDone
+      }
+    }
+    return allPlayersDone
+  }
+  
+  
+  /**************************************
+                  Enemy
+  **************************************/
+  
+  function checkEndEnemyTurn() {
+    let enemyDone = checkEnemyDone()
+    console.log('enemyDone', enemyDone)
+    if (enemyDone) {
+      // gameOverCheck()
+      if (!gameOver) {
+        playerTurn = true
+        playerTurnSetup()
+        let playerTurnText = game.add.text(300, 200, "Player Turn Start");
+        playerTurnText.anchor.set(0.5);
+        playerTurnText.align = 'center';
+        playerTurnText.font = 'Arial';
+        playerTurnText.fontWeight = 'bold';
+        playerTurnText.fontSize = 40;
+        playerTurnText.fill = '#ffffff';
+        playerTurnText.fixedToCamera = true
+        game.time.events.add(Phaser.Timer.SECOND * 1, startPlayerTurn, this, playerTurnText);
+      }
     }
   }
-}
-
-function checkEnemyDone() {
-  let allEnemiesDone = true
-  for(var i = 0, length1 = enemySquad.children.length; i < length1; i++){
-    if(enemySquad.children[i].hasFired === false) {
-      allEnemiesDone = false
-      return allEnemiesDone
+  
+  function checkEnemyDone() {
+    let allEnemiesDone = true
+    for(var i = 0, length1 = enemySquad.children.length; i < length1; i++){
+      if(enemySquad.children[i].hasFired === false) {
+        allEnemiesDone = false
+        return allEnemiesDone
+      }
+    }
+    return allEnemiesDone
+  }
+  
+  function enemyTurnSetup() {
+    for(var i = 0, length1 = enemySquad.children.length; i < length1; i++){
+      enemySquad.children[i].hasMoved = false
+      enemySquad.children[i].hasFired = false
     }
   }
-  return allEnemiesDone
-}
-
-function enemyTurnSetup() {
-  for(var i = 0, length1 = enemySquad.children.length; i < length1; i++){
-    enemySquad.children[i].hasMoved = false
-    enemySquad.children[i].hasFired = false
-  }
-}
-
-
-function startEnemyTurn(enemyTurnText) {
-  if (!victory) {
-    enemyTurnText.destroy()
-    game.world.bringToTop(enemySquad)
-    enemyMoveType()
+  
+  
+  function startEnemyTurn(enemyTurnText) {
+    if (!victory) {
+      enemyTurnText.destroy()
+      game.world.bringToTop(enemySquad)
+      enemyMoveType()
+    }
   }
 }
