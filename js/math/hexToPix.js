cdc8a10757b4325949bc25fec1b32a984326e045
Jufebrown <jufebrown@gmail.com>
2018-02-01 13:47:08 -0600

    exported hexToPix.js

--- js/math/hexToPix.js
+++ js/math/hexToPix.js
@@ -1,17 +1,22 @@
-// functions to convert odd-q hex coodinates to pixel coordinates
+'use strict';
 
-function hexToPixelX(posX) {
-  let pixelX = hexagonWidth/4*3*posX+hexagonWidth/2;
-  return pixelX
+module.exports = function() {
+  // functions to convert odd-q hex coodinates to pixel coordinates
+  
+  function hexToPixelX(posX) {
+    let pixelX = hexagonWidth/4*3*posX+hexagonWidth/2;
+    return pixelX
+  }
+  
+  function hexToPixelY(posX, posY) {
+    let pixelY = hexagonHeight*posY;
+    if(posX%2==0){
+      pixelY += hexagonHeight/2;
+    }
+    else{
+      pixelY += hexagonHeight;
+    }
+    return pixelY
+  }
 }
 
-function hexToPixelY(posX, posY) {
-  let pixelY = hexagonHeight*posY;
-  if(posX%2==0){
-    pixelY += hexagonHeight/2;
-  }
-  else{
-    pixelY += hexagonHeight;
-  }
-  return pixelY
-}
