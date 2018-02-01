5eac164d3911c9eb56ef66ceaa07df1f724b6d72
Jufebrown <jufebrown@gmail.com>
2018-02-01 13:32:24 -0600

    exported addPlayerMechs.js

--- js/game-state/addPlayerMechs.js
+++ js/game-state/addPlayerMechs.js
@@ -1,14 +1,18 @@
-function addPlayerSquad() {
-  let startingPlayerSquadArray = [
-    {positionX: 6, positionY: 6},
-    {positionX: 4, positionY: 6},
-    {positionX: 8, positionY: 6}
-    ]
+'use strict';
 
-  for(let i = 0, length1 = startingPlayerSquadArray.length; i < length1; i++){
-    let startX = hexToPixelX(startingPlayerSquadArray[i].positionX)
-    let startY = hexToPixelY(startingPlayerSquadArray[i].positionX,startingPlayerSquadArray[i].positionY)
-    new Scout(game, startX, startY)
-    playerSquad.children[i].inputEnabled = true
+module.exports = function() {
+  function addPlayerSquad() {
+    let startingPlayerSquadArray = [
+      {positionX: 6, positionY: 6},
+      {positionX: 4, positionY: 6},
+      {positionX: 8, positionY: 6}
+      ]
+  
+    for(let i = 0, length1 = startingPlayerSquadArray.length; i < length1; i++){
+      let startX = hexToPixelX(startingPlayerSquadArray[i].positionX)
+      let startY = hexToPixelY(startingPlayerSquadArray[i].positionX,startingPlayerSquadArray[i].positionY)
+      new Scout(game, startX, startY)
+      playerSquad.children[i].inputEnabled = true
+    }
   }
 }
