053567f73b095d13ae0fb7bbc6862fe9025d9146
Jufebrown <jufebrown@gmail.com>
2018-02-01 13:30:44 -0600

    exported addEnemies.js

--- js/game-state/addEnemies.js
+++ js/game-state/addEnemies.js
@@ -1,14 +1,18 @@
-function addEnemySquad() {
-  let startingEnemySquadArray = [
-    {positionX: 6, positionY: 0},
-    {positionX: 4, positionY: 0},
-    {positionX: 8, positionY: 0}
-    ]
+'use strict';
 
-  for(let i = 0, length1 = startingEnemySquadArray.length; i < length1; i++){
-    let startX = hexToPixelX(startingEnemySquadArray[i].positionX)
-    let startY = hexToPixelY(startingEnemySquadArray[i].positionX,startingEnemySquadArray[i].positionY)
-    new EnemyScout(game, startX, startY)
-    enemySquad.children[i].inputEnabled = true
+module.exports = function() {
+  function addEnemySquad() {
+    let startingEnemySquadArray = [
+      {positionX: 6, positionY: 0},
+      {positionX: 4, positionY: 0},
+      {positionX: 8, positionY: 0}
+      ]
+  
+    for(let i = 0, length1 = startingEnemySquadArray.length; i < length1; i++){
+      let startX = hexToPixelX(startingEnemySquadArray[i].positionX)
+      let startY = hexToPixelY(startingEnemySquadArray[i].positionX,startingEnemySquadArray[i].positionY)
+      new EnemyScout(game, startX, startY)
+      enemySquad.children[i].inputEnabled = true
+    }
   }
 }
