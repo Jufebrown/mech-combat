b75ad4d8e3f7e5dec76478b41df79d3c70ca0dad
Jufebrown <jufebrown@gmail.com>
2018-02-01 14:08:45 -0600

    added indexes requires to app.js

--- js/app.js
+++ js/app.js
@@ -3,11 +3,20 @@ This is a turn-based strategy game that uses a hex
 grid map. The map uses odd-q offset coodinates
 ****************************************************/
 
+require("./combat");
+require("./game-state");
+require("./gui");
+require("./math");
+require("./movement");
+require("./units");
+
 //starts new canvas
 const game = new Phaser.Game(640, 480, Phaser.CANVAS, "game-div", {
-  init: function () {
+  init: function() {
     //initializes kinetic scrolling plugin
-    this.game.kineticScrolling = this.game.plugins.add(Phaser.Plugin.KineticScrolling);
+    this.game.kineticScrolling = this.game.plugins.add(
+      Phaser.Plugin.KineticScrolling
+    );
     this.game.kineticScrolling.configure({
       kineticMovement: true,
       timeConstantScroll: 250,
@@ -16,14 +25,14 @@ const game = new Phaser.Game(640, 480, Phaser.CANVAS, "game-div", {
       horizontalWheel: true,
       verticalWheel: false,
       deltaWheel: 40
-    })
+    });
   },
   preload: onPreload,
   create: onCreate,
-  update: onUpdate,
+  update: onUpdate
 });
 
-let groupOffset = {x: 10, y: 10}
+let groupOffset = { x: 10, y: 10 };
 
 //sets up hex width and height. height should be sqrt(3)/2 of width but need to tweek to get spacing right
 const hexagonHeight = 55;
@@ -33,46 +42,41 @@ const gridSizeX = 50;
 const gridSizeY = 50;
 
 //for mouse position tracking
-const columns = [Math.ceil(gridSizeY/2),Math.floor(gridSizeY/2)];
+const columns = [Math.ceil(gridSizeY / 2), Math.floor(gridSizeY / 2)];
 
 let moveIndex;
-let sectorWidth = hexagonWidth/4*3;
+let sectorWidth = hexagonWidth / 4 * 3;
 let sectorHeight = hexagonHeight;
-let gradient = (hexagonWidth/4)/(hexagonHeight/2);
+let gradient = hexagonWidth / 4 / (hexagonHeight / 2);
 let hexagonGroup;
-let highlightGroup
-let enemyHighlightGroup
-let enemySquad
-let playerSquad
-let craters
-let currentSprite
-let currentHex
-let turnCounter = 1
-let targetFound = false
-let enemyTargetFound = false
-let moveToCubePos
-let victory = false
-let explosions
-let explosionSound
-let playerTurn = true
-let gameOver = false
+let highlightGroup;
+let enemyHighlightGroup;
+let enemySquad;
+let playerSquad;
+let craters;
+let currentSprite;
+let currentHex;
+let turnCounter = 1;
+let targetFound = false;
+let enemyTargetFound = false;
+let moveToCubePos;
+let victory = false;
+let explosions;
+let explosionSound;
+let playerTurn = true;
+let gameOver = false;
 
-
-function onUpdate() {
-
-}
+function onUpdate() {}
 
 function gameOverCheck() {
   if (enemySquad.children.length === 0) {
-    gameOver = true
-    playerWin()
+    gameOver = true;
+    playerWin();
   } else if (playerSquad.children.length === 0) {
-    gameOver = true
-    playerDefeat()
+    gameOver = true;
+    playerDefeat();
   }
 }
-
-
 
 function playerWin() {
   let playerWinText = game.add.text(300, 200, "You Win!");
@@ -82,11 +86,11 @@ function playerWin() {
   // playerWinText.align = 'center';
 
   //  Our font + size
-  playerWinText.font = 'Arial';
-  playerWinText.fontWeight = 'bold';
+  playerWinText.font = "Arial";
+  playerWinText.fontWeight = "bold";
   playerWinText.fontSize = 40;
-  playerWinText.fill = '#ffffff';
-  playerWinText.fixedToCamera = true
+  playerWinText.fill = "#ffffff";
+  playerWinText.fixedToCamera = true;
 
   // playerWinText.cameraOffset.setTo(200, 500);
   // playerWinText.bringToTop()
@@ -120,14 +124,14 @@ function playerDefeat() {
 
   //  Centers the text
   enemyWinText.anchor.set(0.5);
-  enemyWinText.align = 'center';
+  enemyWinText.align = "center";
 
   //  Our font + size
-  enemyWinText.font = 'Arial';
-  enemyWinText.fontWeight = 'bold';
+  enemyWinText.font = "Arial";
+  enemyWinText.fontWeight = "bold";
   enemyWinText.fontSize = 40;
-  enemyWinText.fill = '#ffffff';
-  enemyWinText.fixedToCamera = true
+  enemyWinText.fill = "#ffffff";
+  enemyWinText.fixedToCamera = true;
   // game.lockRender = true
   // enemyWinText.bringToTop()
 
@@ -159,88 +163,98 @@ function enemyMoveType() {
   let enemyMoveTime = 0;
 
   enemySquad.forEach(function(enemySquadMember) {
-    let currentlyMovingEnemy = enemySquadMember
+    let currentlyMovingEnemy = enemySquadMember;
 
-        game.time.events.add(1000 + (enemyMoveTime * 1000), chargeAtPlayer, this, currentlyMovingEnemy);
-        enemyMoveTime++;
-
-    });
+    game.time.events.add(
+      1000 + enemyMoveTime * 1000,
+      chargeAtPlayer,
+      this,
+      currentlyMovingEnemy
+    );
+    enemyMoveTime++;
+  });
 }
 
 function enemyRangeTo(shooter, target) {
-  let targetHexPos = hexPositionFromSpriteCoordinates(target.x, target.y)
-  let targetCubePos = offsetToCube(targetHexPos.x, targetHexPos.y)
-  let shooterHexPos = hexPositionFromSpriteCoordinates(shooter.x, shooter.y)
-  let shooterCubePos = offsetToCube(shooterHexPos.x, shooterHexPos.y)
-  let distanceBetween = cubeDistance(shooterCubePos, targetCubePos)
-  return distanceBetween
+  let targetHexPos = hexPositionFromSpriteCoordinates(target.x, target.y);
+  let targetCubePos = offsetToCube(targetHexPos.x, targetHexPos.y);
+  let shooterHexPos = hexPositionFromSpriteCoordinates(shooter.x, shooter.y);
+  let shooterCubePos = offsetToCube(shooterHexPos.x, shooterHexPos.y);
+  let distanceBetween = cubeDistance(shooterCubePos, targetCubePos);
+  return distanceBetween;
 }
 
 function checkForTargetInWeaponsRange(currentlyMovingEnemy) {
-  for(let i = 0, length1 = playerSquad.children.length; i < length1; i++){
+  for (let i = 0, length1 = playerSquad.children.length; i < length1; i++) {
     if (!currentlyMovingEnemy.hasFired) {
-      let targetDistance = enemyRangeTo(currentlyMovingEnemy, playerSquad.children[i])
+      let targetDistance = enemyRangeTo(
+        currentlyMovingEnemy,
+        playerSquad.children[i]
+      );
       if (targetDistance <= currentlyMovingEnemy.weaponRange) {
-        console.log('checkForTargetInWeaponsRange running')
-        let targetCandidate = playerSquad.children[i]
-        enemyCombat(currentlyMovingEnemy, targetCandidate)
+        console.log("checkForTargetInWeaponsRange running");
+        let targetCandidate = playerSquad.children[i];
+        enemyCombat(currentlyMovingEnemy, targetCandidate);
       } else {
-        currentlyMovingEnemy.hasFired = true
-        checkEndEnemyTurn()
+        currentlyMovingEnemy.hasFired = true;
+        checkEndEnemyTurn();
       }
     }
   }
 }
 
-
-$('.login-page form').submit((e) => {
-  e.preventDefault()
-  var email = $('input[type="email"]').val()
-  var password = $('input[type="password"]').val()
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-  alert(errorMessage)
-})
-  .then(() => {
-    $('form')[0].reset()
-  })
-})
+$(".login-page form").submit(e => {
+  e.preventDefault();
+  var email = $('input[type="email"]').val();
+  var password = $('input[type="password"]').val();
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+      alert(errorMessage);
+    })
+    .then(() => {
+      $("form")[0].reset();
+    });
+});
 
 //sign-out button
-$('.sign-out').click(()=>{
-  firebase.auth().signOut()
-})
+$(".sign-out").click(() => {
+  firebase.auth().signOut();
+});
 
 //register button
-$('.register').click(()=>{
-  var email = $('input[type="email"]').val()
-  var password = $('input[type="password"]').val()
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-    alert(errorMessage)
-  })
-  .then(() => {
-    $('form')[0].reset()
-  })
-})
+$(".register").click(() => {
+  var email = $('input[type="email"]').val();
+  var password = $('input[type="password"]').val();
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+      alert(errorMessage);
+    })
+    .then(() => {
+      $("form")[0].reset();
+    });
+});
 
-$('.main-page form').submit((e) => {
-  e.preventDefault()
-  var task = $('.main-page input[type="text"]').val()
-  var uid = firebase.auth().currentUser.uid
+$(".main-page form").submit(e => {
+  e.preventDefault();
+  var task = $('.main-page input[type="text"]').val();
+  var uid = firebase.auth().currentUser.uid;
   $.post(
     `https://c17-firebase-auth-jufe.firebaseio.com/${uid}.json`,
-    JSON.stringify({task : task})
-  ).then(console.log)
-})
-
-
+    JSON.stringify({ task: task })
+  ).then(console.log);
+});
 
 // //fullscreen function
 // function gofull() {
