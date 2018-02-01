4a421ba41389be9d35de5a7dfd11c66b153f7a81
Jufebrown <jufebrown@gmail.com>
2018-02-01 13:44:55 -0600

    exported cubeOffsetConversions.js

--- js/math/cubeOffsetConversions.js
+++ js/math/cubeOffsetConversions.js
@@ -1,14 +1,18 @@
-function cubeToOffset(x,z) {
-  let offsetCoordinates = {}
-  offsetCoordinates.col = x
-  offsetCoordinates.row = z + (x - (x&1)) / 2
-  return offsetCoordinates
-}
+'use strict';
 
-function offsetToCube (col, row) {
-  let cubeCoordinates = {}
-  cubeCoordinates.x = col
-  cubeCoordinates.z = row - (col - (col&1)) / 2
-  cubeCoordinates.y = -cubeCoordinates.x-cubeCoordinates.z
-  return cubeCoordinates
+module.exports = function() {
+  function cubeToOffset(x,z) {
+    let offsetCoordinates = {}
+    offsetCoordinates.col = x
+    offsetCoordinates.row = z + (x - (x&1)) / 2
+    return offsetCoordinates
+  }
+  
+  function offsetToCube (col, row) {
+    let cubeCoordinates = {}
+    cubeCoordinates.x = col
+    cubeCoordinates.z = row - (col - (col&1)) / 2
+    cubeCoordinates.y = -cubeCoordinates.x-cubeCoordinates.z
+    return cubeCoordinates
+  }
 }
